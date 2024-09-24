1a5c3eb8f935130ae43a679ad5f729b19421a226
Matheus-Nascimento-Angelo <matheus.angelo98@hotmail.com>
2024-09-23 21:40:04 -0300

    Seaborn

--- vizualização_dados/uso_seaborn.py
+++ vizualização_dados/uso_seaborn.py
@@ -13,7 +13,7 @@ plt.show()
 diferentemente dos gráficos plotados utilizando o matplotlib, os dados que são utilizando para a montagem dos gráficos
 do seaborn são declarados dentro da própria função geradora dos gráficos, sendo passados como argumento para o método.
 '''
-# Gráfico de densidade (histograma com suavização nas curvas;
+# Gráfico de densidade (histograma com suavização nas curvas);
 plt.figure(figsize=(6, 10))
 sns.kdeplot(df['salario'], fill=True, color='Blue')
 plt.title('Gráfico de densidade')
@@ -25,3 +25,15 @@ sns.pairplot(df[['idade', 'salario', 'numero_filhos', 'nivel_educacao']])
 plt.show()
 
 # Gráfico de regressão;
+sns.regplot(x='idade', y='salario', data=df, color='Pink', scatter_kws={'alpha':0.5, 'color':'Red'})
+plt.title('Gráfico de regressão')
+plt.xlabel('Idade')
+plt.ylabel('Salário')
+plt.show()
+
+# Gráfico de countplot com hue (agrupamento);
+sns.countplot(x='nivel_educacao', hue='estado_civil', data=df, palette='pastel')
+plt.xlabel('Estado civil')
+plt.ylabel('Nível de educação')
+plt.legend(title='Nível de educação')
+plt.show()
